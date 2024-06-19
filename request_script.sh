e9186068840173431a049d8aee149c3f4fdc2692
Gabriel Pinheiro <g.felippe5965@gmail.com>
2024-06-19 18:33:33 -0300

    test: add test for rate limiter middleware

---
+++ request_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Número total de requisições
+total_requests=51
+
+# URL do endpoint
+endpoint="http://localhost:8080/"
+
+# Header API_KEY
+api_key="4uT1wVB0PLEnGwkT5gl1PZBjd4HCuJA61qYf7FYEORNk95ecp8ixXwTCgsHFvcMLJP9qCyxmHAaKMGC33RxxmSRWIeZujWU51506GmfAR8blDzk7TN5GixXguvx3E2WZ"
+
+# Loop para realizar as requisições com intervalo de 300ms
+for ((i = 1; i <= total_requests; i++)); do
+    # Realiza a requisição HTTP usando curl com o header API_KEY
+    curl -X GET "$endpoint" -H "API_KEY: $api_key" -w "\n" &
+    
+    # Aguarda 300ms (0.3 segundos) antes da próxima requisição
+    sleep 0.3
+done
---
+++ test/internal_middleware_test/rate_limiter_test.go
@@ -0,0 +1,92 @@
+package internal_middleware_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/GabrielFMPinheiro/rate-limiter-full-cycle/api/internal_middleware"
+	"github.com/GabrielFMPinheiro/rate-limiter-full-cycle/infra/cache"
+)
+
+func TestRateLimiterMiddlewareByIP(t *testing.T) {
+	cache := &cache.MockCache{
+		Data: make(map[string]string),
+	}
+	middleware := internal_middleware.NewRateLimiterMiddleware(cache, 5, "./test_api_keys.json")
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Set("API_KEY", "")
+
+	for i := 0; i < 5; i++ {
+		rr := httptest.NewRecorder()
+		middleware.Middleware(handler).ServeHTTP(rr, req)
+		if rr.Code != http.StatusOK {
+			t.Errorf("Expected status OK, got %v", rr.Code)
+		}
+	}
+
+	rr := httptest.NewRecorder()
+	middleware.Middleware(handler).ServeHTTP(rr, req)
+	if rr.Code != http.StatusTooManyRequests {
+		t.Errorf("Expected status Too Many Requests, got %v", rr.Code)
+	}
+}
+
+func TestRateLimiterMiddlewareByAPIKey(t *testing.T) {
+	cache := &cache.MockCache{
+		Data: make(map[string]string),
+	}
+	middleware := internal_middleware.NewRateLimiterMiddleware(cache, 10, "./test_api_keys.json")
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Set("API_KEY", "key1")
+
+	for i := 0; i < 10; i++ {
+		rr := httptest.NewRecorder()
+		middleware.Middleware(handler).ServeHTTP(rr, req)
+		if rr.Code != http.StatusOK {
+			t.Errorf("Expected status OK, got %v", rr.Code)
+		}
+	}
+
+	rr := httptest.NewRecorder()
+	middleware.Middleware(handler).ServeHTTP(rr, req)
+	if rr.Code != http.StatusTooManyRequests {
+		t.Errorf("Expected status Too Many Requests, got %v", rr.Code)
+	}
+}
+
+func TestRateLimiterMiddlewareByAPIKeyNotFound(t *testing.T) {
+	cache := &cache.MockCache{Data: make(map[string]string)}
+	middleware := internal_middleware.NewRateLimiterMiddleware(cache, 5, "./test_api_keys.json")
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Set("API_KEY", "notfound")
+
+	for i := 0; i < 5; i++ {
+		rr := httptest.NewRecorder()
+		middleware.Middleware(handler).ServeHTTP(rr, req)
+		if rr.Code != http.StatusOK {
+			t.Errorf("Expected status OK, got %v", rr.Code)
+		}
+	}
+
+	rr := httptest.NewRecorder()
+	middleware.Middleware(handler).ServeHTTP(rr, req)
+	if rr.Code != http.StatusTooManyRequests {
+		t.Errorf("Expected status Too Many Requests, got %v", rr.Code)
+	}
+}
